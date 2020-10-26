467349cbef0c56bc63aaf4f09971642f59a7bf7e
vagrant <avedj94@hotmail.com>
2020-10-26 23:59:02 +0000

    Revised for loop

--- tail.js
+++ tail.js
@@ -6,21 +6,37 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+
 let newArray = [];
 const tail = function(input) {
   if (input.length <= 1) {
     newArray = [];
   } else {
-    for (let i = input.length; i <= 1; i++) {
-      newArray.push(i);
+    for (let i = 1; i <= input.length; i++) {
+      newArray.push(input[i]);
     }
   }
   return newArray;
 };
 
+/*
+const tail = function(input) {
+  if (input.length === 0) return [];
+  if (input.length === 1) return input;
+  input.shift();
+  return input;
+}
+*/
+
+
+
+
+
+
+
 
 // Test Case: Check the original array
-const words = ["Yo Yo"];
+const words = ["Yo Yo Else Hello", "df", 'asdf'];
 tail(words); // no need to capture the return value since we are not checking it
 assertEqual(words.length, 3); // original array should still have 3 elements!
 assertEqual(newArray.length, 1);
