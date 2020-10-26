24d42b24ae792b7b926d189e04e13de3527a3d53
vagrant <avedj94@hotmail.com>
2020-10-26 20:03:41 +0000

    compare two values and tell us if they match or not

---
+++ assertEqual.js
@@ -0,0 +1,13 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅👻 Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("🛑🛑🛑💩 Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 8);
