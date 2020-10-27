f652fc5f391a0c373c3a5ba5c512c144c16d18e9
vagrant <avedj94@hotmail.com>
2020-10-27 19:01:32 +0000

    Eliminated repeated code

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,17 +1,21 @@
-const assertArraysEqual = function (array1, array2) {
+const eqArrays = function (array1, array2) {
   let count = 0;
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] === array2[i]) {
       count++;
     }
   }
-  if (count === array1.length && count === array2.length) {
+  if (count === array1.length && count === array2.length) return true;
+  else return false;
+};
+
+const assertArraysEqual = function (array1, array2) {
+  if (eqArrays(array1, array2)) {
     console.log(`✅✅✅👻 Assertion Passed: ${array1} === ${array2}`);
   } else {
     console.log(`🛑🛑🛑💩 Assertion Failed: ${array1} !== ${array2}`);
   }
 };
-
 // TEST CODE
 assertArraysEqual ([1, 2, 3], [1, 2, 3]); 
 assertArraysEqual ([1, 2, 3], [3, 2, 1]);
