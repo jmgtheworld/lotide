74e319e3794c32ee98579f64ae6933d7cdcc1d05
vagrant <avedj94@hotmail.com>
2020-10-28 23:47:23 +0000

    check for length earlier

--- eqArrays.js
+++ eqArrays.js
@@ -8,21 +8,24 @@ const assertEqual = function(actual, expected) {
 
 const eqArrays = function (array1, array2) {
   let count = 0;
+  if (array1.length !== array2.length) return false;
+
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] === array2[i]) {
       count++;
     }
   }
-  if (count === array1.length && count === array2.length) return true;
-  else return false;
+  if (count !== array1.length || count !== array2.length) return false;
   
-}
+  return true;
+};
+
 
 // TEST CODE
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
 assertEqual(eqArrays([1, 2, 3], [1, 2, '3']), false);
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), false);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4, 5]), false);
 assertEqual(eqArrays(['',false, 0], [null, false, NaN]), false);
 
 
