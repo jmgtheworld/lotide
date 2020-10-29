2d71730d42639355b042ee4dd19adb8722ecc7de
vagrant <avedj94@hotmail.com>
2020-10-29 20:04:14 +0000

    assert equal edit

--- findKey.js
+++ findKey.js
@@ -18,7 +18,6 @@ const findKey = function (object, callback) {
   }
 }
 
-
 console.log(findKey({
   "Blue Hill": { stars: 1 },
   "Akaleri":   { stars: 3 },
@@ -27,3 +26,27 @@ console.log(findKey({
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
 }, x => x.stars === 2)) // => "noma"
+
+console.log(findKey({
+  "Michael": { hotness: 10 },
+  "Jim": { hotness: 8 },
+  "Pam": { hotness: 9 },
+  "Tobu": { hotness: 4 },
+}, x => x.hotness === 10)) // => "noma"
+
+// TEST CODE
+assertEqual(findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2), "noma");
+
+assertEqual(findKey({
+  "Michael": { hotness: 10 },
+  "Jim": { hotness: 8 },
+  "Pam": { hotness: 9 },
+  "Tobu": { hotness: 4 },
+}, x => x.hotness === 10), "Michael");
