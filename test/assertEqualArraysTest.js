97e615db3d8aa5d40a57d5b609a80c60d568c184
vagrant <avedj94@hotmail.com>
2020-11-02 15:57:01 +0000

    refactored eqarrays test

---
+++ test/assertEqualArraysTest.js
@@ -0,0 +1,10 @@
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
+
+
+// TEST CODE
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); 
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false);
+assertEqual(eqArrays([1, 2, 3], [1, 2, '3']), false);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4, 5]), false);
+assertEqual(eqArrays(['',false, 0], [null, false, NaN]), false);
