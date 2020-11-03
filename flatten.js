3802dd8eec92df3e717bec8c6dacb6930296037b
vagrant <avedj94@hotmail.com>
2020-11-03 01:12:00 +0000

    refactor using mocha, chai

--- flatten.js
+++ flatten.js
@@ -1,21 +1,4 @@
-const eqArrays = function (array1, array2) {
-  let count = 0;
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] === array2[i]) {
-      count++;
-    }
-  }
-  if (count === array1.length && count === array2.length) return true;
-  else return false;
-};
-
-const assertArraysEqual = function (array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅✅✅👻 Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`🛑🛑🛑💩 Assertion Failed: ${array1} !== ${array2}`);
-  }
-};
+const assertArraysEqual = require('./assertArraysEqual');
 
 const flatten = function (array) {
   let newArray = [];
@@ -32,5 +15,4 @@ const flatten = function (array) {
   return newArray;
 }
 
-
-assertArraysEqual (flatten([1, 2, [3, 4], 5, [6]]),[1, 2, 3, 4, 5, 6])
+module.exports = flatten;
