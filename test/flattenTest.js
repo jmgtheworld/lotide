8cab06d7a84a8bef314d1f296904bf28459de950
vagrant <avedj94@hotmail.com>
2020-11-03 01:03:48 +0000

    refactor using mocha,chai

---
+++ test/flattenTest.js
@@ -0,0 +1,9 @@
+const assert = require('chai').assert;
+const flatten = require('../flatten');
+
+describe('#flatten', () => {
+  it("should return [1, 2, 3, 4, 5, 6] when input is [1, 2, [3, 4], 5, [6]]", 
+  () => {
+    assert.deepEqual(flatten([1, 2, [3, 4], 5, [6]]),[1, 2, 3, 4, 5, 6]);
+  });
+});
