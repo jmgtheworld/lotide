be349ab3c1c3ce8ca034e6dfec949d0cf6714416
vagrant <avedj94@hotmail.com>
2020-11-03 01:11:04 +0000

    refactor using mocha, chai

--- letterPositions.js
+++ letterPositions.js
@@ -1,21 +1,4 @@
-const eqArrays = function (array1, array2) {
-  let count = 0;
-  for (let i = 0; i < array1.length; i++) {
-    if (array1[i] === array2[i]) {
-      count++;
-    }
-  }
-  if (count === array1.length && count === array2.length) return true;
-  else return false;
-};
-
-const assertArraysEqual = function (array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`✅✅✅👻 Assertion Passed: ${array1} === ${array2}`);
-  } else {
-    console.log(`🛑🛑🛑💩 Assertion Failed: ${array1} !== ${array2}`);
-  }
-};
+const assertArraysEqual = require('./assertArraysEqual');
 
 const letterPositions = function(sentence) {
   const results = {};
@@ -34,10 +17,4 @@ const letterPositions = function(sentence) {
   return results;
 };
 
-
-// TEST CODE
-console.log(letterPositions("lighthouse in the house"))
-assertArraysEqual(letterPositions("helllo").h, [0]);
-assertArraysEqual(letterPositions("helllo").e, [1]);
-assertArraysEqual(letterPositions("helllo").l, [2,3,4]);
-assertArraysEqual(letterPositions("helllo").o, [5]);
+module.exports = letterPositions;
